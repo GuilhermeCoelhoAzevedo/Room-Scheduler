65fdb38b88aa5498c90a6b3b18ac375d5fab1bed
Guilherme Coelho de Azevedo <guilhermecoelhodeazevedo@gmail.com>
2021-04-09 15:18:37 +0100

    Bracket 3 - Booking UI and booking validations

--- application/forms.py
+++ application/forms.py
@@ -2,6 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, IntegerField
 from wtforms.fields.html5 import DateField, TimeField
 from wtforms.validators import DataRequired, ValidationError, Length
+from datetime import datetime
+import pytz
+from flask import flash
 from application import client
 
 class roomForm(FlaskForm):
@@ -17,3 +20,110 @@ class roomForm(FlaskForm):
         #VALIDATION CREATED FOR ADD/EDIT GPU
         if room:
             raise ValidationError("Room already exists in the system!")
+
+class bookingForm(FlaskForm):
+    id_hidden     = IntegerField("Id")
+    room_number   = IntegerField("Room number")
+    dt_start      = DateField("Start date", format='%Y-%m-%d')
+    hr_start      = TimeField('Start time', format='%H:%M')
+    dt_finish     = DateField("Finish date", format='%Y-%m-%d')
+    hr_finish     = TimeField('Finish time', format='%H:%M')
+    submit        = SubmitField("Reserve")
+
+    def validate(self):
+        result = True
+
+        if not self.dt_start.data:
+            errors = list(self.dt_start.errors)
+            errors.append("This field is required.")
+            errors = tuple(errors)
+            self.dt_start.errors = errors
+
+            result = False  
+
+        if not self.dt_finish.data:
+            errors = list(self.dt_finish.errors)
+            errors.append("This field is required.")
+            errors = tuple(errors)
+            self.dt_finish.errors = errors
+
+            result = False  
+
+        if not self.hr_start.data:
+            errors = list(self.hr_start.errors)
+            errors.append("This field is required.")
+            errors = tuple(errors)
+            self.hr_start.errors = errors
+
+            result = False  
+
+        if not self.hr_finish.data:
+            errors = list(self.hr_finish.errors)
+            errors.append("This field is required.")
+            errors = tuple(errors)
+            self.hr_finish.errors = errors
+
+            result = False
+
+        if not result:
+            return False
+
+        now         = datetime.now()
+        dt_start    = datetime.combine(self.dt_start.data, self.hr_start.data)
+        dt_finish   = datetime.combine(self.dt_finish.data, self.hr_finish.data)
+
+        if dt_start < now:
+            errors = list(self.dt_start.errors)
+            errors.append("Start date in the past is not valid!")
+            errors = tuple(errors)
+            self.dt_start.errors = errors
+
+            result = False
+
+        if dt_finish < now:
+            errors = list(self.dt_finish.errors)
+            errors.append("Finish date in the past is not valid!")
+            errors = tuple(errors)
+            self.dt_finish.errors = errors
+
+            result = False
+
+        if not result:
+            return result
+
+        if dt_finish <= dt_start:
+            errors = list(self.dt_finish.errors)
+            errors.append("Finish date must be greather than start date!")
+            errors = tuple(errors)
+            self.dt_finish.errors = errors
+            
+            return False
+
+        entity_key  = client.key("Room", int(self.room_number.data))
+        room = client.get(entity_key)
+
+        #VALIDATING IF THERE IS A BOOKING FOR THE CHOSEN DATES
+        query = client.query(kind='Booking')
+        query.add_filter("Room", "=", room.key)
+        bookingData = list(query.fetch())
+        
+        current_booked  = False
+        utc             = pytz.UTC
+
+        for booking in bookingData:
+            if booking.key.id == int(self.id_hidden.data):
+                continue
+
+            if dt_start.replace(tzinfo=utc) <= booking['dt_start']  <= dt_finish.replace(tzinfo=utc):
+                current_booked = True
+
+            if dt_start.replace(tzinfo=utc) <= booking['dt_finish'] <= dt_finish.replace(tzinfo=utc):
+                current_booked = True
+
+        if current_booked:
+            flash("This room is already booked for the chosen dates!", "danger")
+
+            result = False
+            
+        return result
+            
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,8 @@
 from application import app, client
 from flask import Flask, render_template, request, session, url_for, redirect, flash, json, jsonify
-from application.forms import roomForm
+from application.forms import roomForm, bookingForm
+from datetime import datetime
+import pytz
 
 from google.cloud import datastore
 import google.oauth2.id_token
@@ -95,3 +97,112 @@ def room():
         return redirect(url_for('index'))
 
     return render_template('room.html', room=True, form=form)
+
+@app.route("/bookings", methods=['GET', 'POST'])
+def bookings():
+    #CHECK IF USER IS LOGGED IN
+    if not session.get('email'):
+        return redirect(url_for("login"))
+
+    #AJAX FOR BOOKINGS FILTERS
+    if request.method == 'POST':
+        if request.is_json:
+            filters = request.get_json(force=True)
+
+            booking_list    = []
+            query           = client.query(kind='Booking')
+            utc             = pytz.UTC
+
+            #FILTERING ROOM
+            if filters["room_number"]:
+                entity_key  = client.key("Room", int(filters["room_number"]))
+                query.add_filter("Room", "=", entity_key)
+
+            #FILTERING UER
+            if filters["user_bookings"]:
+                entity_key  = client.key("User", int(session['id']))
+                query.add_filter("User", "=", entity_key)
+
+            bookingData = query.fetch()
+
+            for booking in bookingData:
+                #FILTERING START DATE
+                if filters["dt_start"]:
+                    dt_start = datetime.strptime(filters["dt_start"], '%Y-%m-%d').replace(tzinfo=utc)
+                    if booking['dt_start'] < dt_start:
+                        continue
+                
+                #FILTERING FINISH DATE
+                if filters["dt_finish"]:
+                    dt_finish = datetime.strptime(filters["dt_finish"]+" 23:59:59", '%Y-%m-%d %H:%M:%S').replace(tzinfo=utc)
+                    if booking['dt_finish'] > dt_finish:
+                        continue
+                
+                user = client.get(booking['User'])
+
+                if user.key.id == session['id']:
+                    same_user = True
+                else:
+                    same_user = False
+
+                room        = client.get(booking['Room'])
+                dt_start    = booking['dt_start'].strftime("%d/%m/%Y %H:%M:%S")
+                dt_finish   = booking['dt_finish'].strftime("%d/%m/%Y %H:%M:%S")
+                booking_list.append([room.key.id, room['name'], dt_start, dt_finish, str(booking.key.id), user['email'], same_user])
+                
+            results = json.dumps(booking_list)
+
+            return results
+
+    query       = client.query(kind='Booking')
+    bookingData = list(query.fetch())
+    
+    for element in bookingData:
+        element['User'] = client.get(element['User'])
+        element['Room'] = client.get(element['Room'])
+        element['dt_start'] = element['dt_start'].strftime("%d/%m/%Y %H:%M:%S")
+        element['dt_finish'] = element['dt_finish'].strftime("%d/%m/%Y %H:%M:%S")
+
+    return render_template('bookings.html', bookings=True, bookingData=bookingData)
+
+@app.route("/newBooking/<roomNumber>", methods=['GET', 'POST'])
+def newBooking(roomNumber):
+    #CHECK IF USER IS LOGGED IN
+    if not session.get('email'):
+        return redirect(url_for("login"))
+
+    entity_key  = client.key("Room", int(roomNumber))
+    room        = client.get(entity_key)
+
+    if not room:
+        return redirect(url_for("index"))
+
+    form                    = bookingForm()
+    form.room_number.data   = room.key.id
+    form.id_hidden.data     = "0"
+    title                   = "New booking: Room " + str(room.key.id)
+
+    #INSERT ROOM IN THE DATABASE
+    if form.validate_on_submit():
+        dt_start    = form.dt_start.data
+        dt_finish   = form.dt_finish.data
+        hr_start    = form.hr_start.data
+        hr_finish   = form.hr_finish.data
+        user        = client.key("User", session.get('id'))
+
+        booking = datastore.Entity(key = client.key('Booking'))
+        
+        booking.update({
+            'dt_start' : datetime.combine(dt_start, hr_start),
+            'dt_finish' : datetime.combine(dt_finish, hr_finish),
+            'Room' : room.key,
+            'User' : user
+        })
+
+        client.put(booking)
+
+        flash(f"Booking in the room {str(room.key.id)} was successfully included!", "success")
+        
+        return redirect(url_for('bookings'))
+
+    return render_template('booking.html', form=form, title=title)
