acfd66fc8dcc2b8d9b9f9c57c4812d6811696037
Guilherme Coelho de Azevedo <guilhermecoelhodeazevedo@gmail.com>
2021-04-27 23:39:10 +0100

    Taking out Google credentials

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or b'-\xbf\x96OA\xec\xber\\~\xe1\xf1\xa6\x9f\x8d\xddA\xf2\xf6\x8c\xe8n!Z'
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= "E:\\Griffith\\CPA\\room-locator-e80d4ee86476.json"
+    #os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= "E:\\Griffith\\CPA\\room-locator-e80d4ee86476.json"
     
